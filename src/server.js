85ff3b0766c42387b1e4852694e5730525e67d88
@fmota-dev <fmota.web@gmail.com>
2024-05-31 13:09:29 -0300

    pasta estatica docs

--- src/server.js
+++ src/server.js
@@ -2,19 +2,25 @@ import express from 'express';
 import cors from 'cors';
 import userRoutes from './routes/userRoutes.js';
 import docsRoutes from './routes/docsRoutes.js';
+
 import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.use(express.static('public/docs'))
+app.use(express.static('public/docs'));
 
 // Configurando as rotas
 
 app.get('/', (req, res) => {
-	res.sendFile(path.join(__dirname, 'public/docs', 'index.html'));
+	res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
 app.use(userRoutes);
