003bb099b6287be1e58ae2df9f6e0caceea15f83
@fmota-dev <fmota.web@gmail.com>
2024-06-08 00:14:21 -0300

    feat: adicionado zod para validação

--- src/middlewares/userMiddleware.js
+++ src/middlewares/userMiddleware.js
@@ -1,39 +1,29 @@
-// src/middleware.js
+import { z } from "zod";
+
+const userSchema = z.object({
+  name: z.string().min(1, "Nome inválido"),
+  email: z.string().email("Email inválido"),
+  age: z.number().positive("Idade inválida"),
+});
 
 const validateCreateUser = (req, res, next) => {
-	const { name, email, age } = req.body;
-
-	if (typeof name !== 'string' || name.trim() === '') {
-		return res.status(400).json({ error: 'Nome inválido' });
-	}
-
-	if (typeof email !== 'string' || !/\S+@\S+\.\S+/.test(email)) {
-		return res.status(400).json({ error: 'Email inválido' });
-	}
-
-	if (typeof age !== 'number' || age < 0) {
-		return res.status(400).json({ error: 'Idade inválida' });
-	}
-
-	next();
+  try {
+    userSchema.parse(req.body);
+    next();
+  } catch (error) {
+    const errorMessage = error.errors.map((err) => err.message);
+    return res.status(400).json({ error: errorMessage });
+  }
 };
 
 const validateUpdateUser = (req, res, next) => {
-	const { name, email, age } = req.body;
-
-	if (name && (typeof name !== 'string' || name.trim() === '')) {
-		return res.status(400).json({ error: 'Nome inválido' });
-	}
-
-	if (email && (typeof email !== 'string' || !/\S+@\S+\.\S+/.test(email))) {
-		return res.status(400).json({ error: 'Email inválido' });
-	}
-
-	if (age && (typeof age !== 'number' || age < 0)) {
-		return res.status(400).json({ error: 'Idade inválida' });
-	}
-
-	next();
+  try {
+    userSchema.partial().parse(req.body);
+    next();
+  } catch (error) {
+    const errorMessage = error.errors.map((err) => err.message);
+    return res.status(400).json({ error: errorMessage });
+  }
 };
 
 export { validateCreateUser, validateUpdateUser };
